e69320e99f58fc4fa0e11d81d12a0da2061de721
whisky81 <anh666666anh@gmail.com>
2025-06-08 14:56:45 +0700

    test rating.dat

--- Assignment1Tester.py
+++ Assignment1Tester.py
@@ -10,8 +10,8 @@ RROBIN_TABLE_PREFIX = 'rrobin_part'
 USER_ID_COLNAME = 'userid'
 MOVIE_ID_COLNAME = 'movieid'
 RATING_COLNAME = 'rating'
-INPUT_FILE_PATH = 'test_data.dat'
-ACTUAL_ROWS_IN_INPUT_FILE = 20  # Number of lines in the input file
+INPUT_FILE_PATH = './ml-10M100K/ratings.dat'
+ACTUAL_ROWS_IN_INPUT_FILE = 10_000_054  # Number of lines in the input file
 
 import psycopg2
 import traceback
